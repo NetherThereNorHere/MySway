aeee6923d001db59b9e7b8e03fc4ef531f76a4bd
NetherThereNorHere <benjaminhalvorsen@gmail.com>
2026-05-06 23:09:04 +0000

    Update fzf.zsh

--- files/system/etc/zsh/zshrc.d/fzf.zsh
+++ files/system/etc/zsh/zshrc.d/fzf.zsh
@@ -1,1 +1,7 @@
 source <(fzf --zsh)
+
+# Set bat as the default previewer for fzf file searches
+export FZF_DEFAULT_OPTS="--preview 'bat --color=always --style=numbers --line-range :500 {}'"
+
+# Handy alias: 'fp' (file preview)
+alias fp='fzf --preview "bat --color=always --style=header,grid --line-range :300 {}"'
