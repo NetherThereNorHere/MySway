ec8948f434250cfc8f3b67e74bd9155ef34022aa
NetherThereNorHere <benjaminhalvorsen@gmail.com>
2026-05-06 22:51:29 +0000

    Create history.zsh

---
+++ files/system/etc/zsh/zshrc.d/history.zsh
@@ -0,0 +1,20 @@
+HISTFILE="$HOME/.zsh_history"
+
+HISTSIZE=2000
+SAVEHIST=1000
+
+setopt INC_APPEND_HISTORY
+setopt SHARE_HISTORY
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+
+# Ignores adding commands to the history file, but keeps them in the current terminal's history
+zshaddhistory() {
+    # Add app names here separated by |
+    # Example: don't save history for 'python' or 'nmap'
+    local ignore_apps='(aria2c|mpv|ytd)'
+    
+    if [[ "$1" =~ ^$ignore_apps ]]; then
+        return 1 # Tells Zsh: "Keep it in memory for this session, but don't write to disk"
+    fi
+}
