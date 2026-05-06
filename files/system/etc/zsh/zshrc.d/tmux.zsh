c044eeb17701ec462c27af1ae94cba85db2e79a2
NetherThereNorHere <benjaminhalvorsen@gmail.com>
2026-05-06 16:31:37 +0000

    Update tmux.zsh

--- files/system/etc/zsh/zshrc.d/tmux.zsh
+++ files/system/etc/zsh/zshrc.d/tmux.zsh
@@ -1,5 +1,5 @@
 # Only start tmux if we are in an interactive shell and not already inside tmux
 if [[ -z "$TMUX" && $- == *i* ]]; then
     # Try to attach to a session named 'main', or create it if it doesn't exist
-    exec tmux attach-session -t main || tmux new-session -s main
+    tmux attach-session -t main || tmux new-session -s main
 fi
