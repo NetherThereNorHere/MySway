07a9cd55cf9edd28705a3cd41519a52bc95f1f8c
NetherThereNorHere <benjaminhalvorsen@gmail.com>
2026-05-07 15:17:59 +0000

    Update colors.zsh

--- files/system/etc/zsh/zshrc.d/colors.zsh
+++ files/system/etc/zsh/zshrc.d/colors.zsh
@@ -1,4 +1,23 @@
-# Import the current colorscheme variables
-[ -f ~/.cache/wal/colors.sh ] && source ~/.cache/wal/colors.sh
+# Source the manual color choices
+[ -f ~/.config/theme/colors.sh ] && source ~/.config/theme/colors.sh
 
-# Now you can use $color0 through $color15 in any script or alias
+# Assign the variables to the names your apps expect
+export THEME_BG=$color0
+export THEME_FG=$color7
+export THEME_INSERT=$color2
+export THEME_NORMAL=$color3
+export THEME_ACCENT=$color4
+
+# Sync these to tmux environment immediately
+if [ -n "$TMUX" ]; then
+    tmux set-environment THEME_BG "$THEME_BG"
+    tmux set-environment THEME_NORMAL "$THEME_NORMAL"
+    tmux set-environment THEME_INSERT "$THEME_INSERT"
+fi
+
+# Command that applies colors from ~/.config/theme/colors.sh
+apply-theme() {
+    source /etc/zsh/zshrc.d/colors.zsh
+    tmux source-file /etc/tmux.conf
+    echo "Theme applied!"
+}
