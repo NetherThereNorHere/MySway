62c72530ac0ba34ab0e1ff07f1aa9d3020177a07
NetherThereNorHere <benjaminhalvorsen@gmail.com>
2026-05-11 19:14:36 +0000

    Delete files/system/etc/zsh/zshrc.d/tmux.zsh

--- files/system/etc/zsh/zshrc.d/tmux.zsh
+++
@@ -1,5 +0,0 @@
-# Only start tmux if we are in an interactive shell and not already inside tmux
-if [[ -z "$TMUX" && $- == *i* ]]; then
-    # Try to attach to a session named 'main', or create it if it doesn't exist
-    tmux attach-session -t main || tmux new-session -s main
-fi
