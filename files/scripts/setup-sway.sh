f339fb0d203e15ed647481214f6ec35cbaab7ede
NetherThereNorHere <benjaminhalvorsen@gmail.com>
2026-05-11 22:55:00 +0000

    Update setup-sway.sh

--- files/scripts/setup-sway.sh
+++ files/scripts/setup-sway.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Configuring Sway autostart..."
-cat <<EOF >> /etc/zprofile
+echo "Configuring Sway autostart for new users..."
+
+# Ensure the file exists before appending
+touch /etc/skel/.zprofile
+
+cat <<EOF >> /etc/skel/.zprofile
 
 # Autostart Sway on TTY1
 if [[ -z \$DISPLAY ]] && [[ \$(tty) = /dev/tty1 ]]; then
